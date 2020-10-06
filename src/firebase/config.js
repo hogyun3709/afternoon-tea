29d764e226354e6aae0827e31cb9dde5efa8c67b
hogyun3709 <hogyun3709@gmail.com>
2020-10-06 18:16:00 +0900

    Delete dotenv usage - RN doenst support npm standard libs

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,26 @@
 import * as firebase from 'firebase';
 import '@firebase/auth';
 import '@firebase/firestore';
-require('dotenv').config();
+// require('dotenv').config();
+//
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_NATIVE_APP_API_KEY,
+//   authDomain: process.env.REACT_NATIVE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_NATIVE_DATABASE_URL,
+//   projectId: process.env.REACT_NATIVE_PROJECT_ID,
+//   storageBucket: process.env.REACT_NATIVE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_NATIVE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_NATIVE_APP_ID
+// };
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_NATIVE_APP_API_KEY,
-  authDomain: process.env.REACT_NATIVE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_NATIVE_DATABASE_URL,
-  projectId: process.env.REACT_NATIVE_PROJECT_ID,
-  storageBucket: process.env.REACT_NATIVE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_NATIVE_ MESSAGING_SENDER_ID,
-  appId: process.env.REACT_NATIVE_APP_ID
+  apiKey: 'AIzaSyCnKeoY-qE3CyK_zXu7wpghMdpJN100tE8',
+  authDomain: 'afternoon-tea-789df.firebaseapp.com',
+  databaseURL: 'https://afternoon-tea-789df.firebaseio.com',
+  projectId: 'afternoon-tea-789df',
+  storageBucket: 'afternoon-tea-789df.appspot.com',
+  messagingSenderId: '678571585956',
+  appId: '1:678571585956:ios:430cd88b8e033036f41131'
 };
 
 if (!firebase.apps.length) {
